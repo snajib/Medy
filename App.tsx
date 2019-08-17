3185f84f12a28026095a84964ada4bc6b6856f4b
Refai <zhrefai@ou.edu>
2019-08-16 23:54:25 -0500

    add basic bg color and card

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ViewStyle, TextStyle } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>test</Text>
-    </View>
-  );
+   return (
+      <View style={styles.containerStyle}>
+         <View style={styles.cardStyle} />
+      </View>
+   );
 }
 
+const containerStyle: ViewStyle = {
+   flex: 1,
+   backgroundColor: "#6296E8",
+   alignItems: "center",
+   justifyContent: "center"
+};
+
+const textStyle: TextStyle = {
+   color: "red"
+};
+
+const cardStyle: ViewStyle = {
+   backgroundColor: "#fff",
+   borderRadius: 12,
+   height: "70%",
+   width: "65%"
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   containerStyle,
+   cardStyle,
+   textStyle
 });
