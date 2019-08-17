58e33bd773dc45e39aeefbcaaf3e6c3bf915aaa2
Refai <zhrefai@ou.edu>
2019-08-17 13:32:30 -0500

    add gradient

--- App.tsx
+++ App.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { StyleSheet, Text, View, ViewStyle, TextStyle } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
    return (
       <View style={styles.containerStyle}>
+         <LinearGradient
+            colors={["rgba(86,141,230,1)", "transparent"]}
+            style={{
+               position: "absolute",
+               left: 0,
+               right: 0,
+               top: 0,
+               height: 1200
+            }}
+         />
          <View style={styles.cardStyle} />
       </View>
    );
 }
 
+//Background Color Style
 const containerStyle: ViewStyle = {
    flex: 1,
-   backgroundColor: "#6296E8",
+   backgroundColor: "#5F44BA",
    alignItems: "center",
    justifyContent: "center"
 };
 
+//Text Styles
 const textStyle: TextStyle = {
    color: "red"
 };
 
+//Card Style and format
 const cardStyle: ViewStyle = {
    backgroundColor: "#fff",
    borderRadius: 12,
@@ -28,10 +42,20 @@ const cardStyle: ViewStyle = {
    shadowOpacity: 0.8,
    shadowRadius: 3,
    height: "70%",
-   width: "65%"
+   width: "67%"
 };
+
+const linearGradient: ViewStyle = {
+   flex: 1,
+   paddingLeft: 15,
+   paddingRight: 15,
+   borderRadius: 5
+};
+
+//Create all styles above
 const styles = StyleSheet.create({
    containerStyle,
    cardStyle,
-   textStyle
+   textStyle,
+   linearGradient
 });
