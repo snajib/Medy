d6c5cca3c5f750cc19e3c48da23d1630fcd7e7ef
Refai <zhrefai@ou.edu>
2019-08-19 22:55:11 -0500

    add tabs and adj taskbar

--- App.tsx
+++ App.tsx
@@ -1,57 +1,8 @@
 import React from "react";
+import AppContainer from "./EmployerFinderScreen";
 import { StyleSheet, Text, View, ViewStyle, TextStyle } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-   return (
-      <View style={styles.containerStyle}>
-         <LinearGradient
-            colors={["rgba(86,141,230,1)", "transparent"]}
-            style={{
-               position: "absolute",
-               left: 0,
-               right: 0,
-               top: 0,
-               height: 1200
-            }}
-         />
-         <View style={styles.cardStyle} />
-      </View>
-   );
+   return <AppContainer />;
 }
-
-const containerStyle: ViewStyle = {
-   flex: 1,
-   backgroundColor: "#5F44BA",
-   alignItems: "center",
-   justifyContent: "center"
-};
-
-const textStyle: TextStyle = {
-   color: "red"
-};
-
-const cardStyle: ViewStyle = {
-   backgroundColor: "#fff",
-   borderRadius: 12,
-   shadowColor: "#000",
-   shadowOffset: { width: 0, height: 3 },
-   shadowOpacity: 0.8,
-   shadowRadius: 3,
-   height: "70%",
-   width: "67%"
-};
-
-const linearGradient: ViewStyle = {
-   flex: 1,
-   paddingLeft: 15,
-   paddingRight: 15,
-   borderRadius: 5
-};
-
-const styles = StyleSheet.create({
-   containerStyle,
-   cardStyle,
-   textStyle,
-   linearGradient
-});
---
+++ EmployerFinderScreen.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { StyleSheet, Text, View, ViewStyle, TextStyle } from "react-native";
+import {
+   createStackNavigator,
+   createAppContainer,
+   createBottomTabNavigator
+} from "react-navigation";
+import { LinearGradient } from "expo-linear-gradient";
+import EmployerPostingsScreen from "./EmployerPostingsScreen";
+import EmployeeCalenderScreen from "./EmployeeCalenderScreen";
+import EmployeeMoreScreen from "./EmployeeMoreScreen";
+
+class EmployerFinderScreen extends React.Component {
+   render() {
+      return (
+         <View style={styles.containerStyle}>
+            <LinearGradient
+               colors={["rgba(86,141,230,1)", "transparent"]}
+               style={{
+                  position: "absolute",
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  height: 1200
+               }}
+            />
+            <View style={styles.cardStyle}>
+               <View style={styles.centerText}>
+                  <Text>Home</Text>
+               </View>
+            </View>
+         </View>
+      );
+   }
+}
+
+const containerStyle: ViewStyle = {
+   flex: 1,
+   backgroundColor: "#5F44BA",
+   alignItems: "center",
+   justifyContent: "center"
+};
+
+const centerText: ViewStyle = {
+   flex: 1,
+   alignItems: "center",
+   justifyContent: "center"
+};
+
+const cardStyle: ViewStyle = {
+   backgroundColor: "#fff",
+   borderRadius: 12,
+   shadowColor: "#000",
+   shadowOffset: { width: 0, height: 3 },
+   shadowOpacity: 0.8,
+   shadowRadius: 3,
+   height: "70%",
+   width: "67%"
+};
+
+const linearGradient: ViewStyle = {
+   flex: 1,
+   paddingLeft: 15,
+   paddingRight: 15,
+   borderRadius: 5
+};
+
+const styles = StyleSheet.create({
+   containerStyle,
+   cardStyle,
+   linearGradient,
+   centerText
+});
+
+/*TODO: Find a way to create a stack for each screen
+   Anything that transitions from the original page of the tab to a separate
+   page within the original page of a tab requires a stack
+
+   Exs. 
+      Super list inside of Findings tab
+      View posting inquiries inside of Postings tab
+      View employee cards or QR codes from Calender tab
+      View other options in the more tab (micro transactions)
+*/
+const TabNavigator = createBottomTabNavigator(
+   {
+      Home: { screen: EmployerFinderScreen },
+      Postings: { screen: EmployerPostingsScreen },
+      Calender: { screen: EmployeeCalenderScreen },
+      More: { screen: EmployeeMoreScreen }
+   },
+   {
+      tabBarOptions: {
+         style: { paddingBottom: 10, height: 70 }
+      }
+   }
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default AppContainer;
