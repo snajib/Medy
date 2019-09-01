f835e233745f60b126fb0e64b2295d610ee16126
Zaki <z.refai@optecks.com>
2019-09-01 02:00:55 -0400

    Added custom drawer component

---
+++ CustomDrawerComponent.js
@@ -0,0 +1,166 @@
+import React from "react";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createDrawerNavigator,
+  createStackNavigator,
+  DrawerItems,
+  NavigationEvents
+} from "react-navigation";
+
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  
+} from "react-native";
+
+export class CustomDrawerComponent extends React.Component {
+  constructor(props) {
+      super(props);        
+      this.state = {};
+    }
+    
+  render() {        
+    const { navigate } = this.props.navigation;
+    return (
+      
+      <SafeAreaView style={{flex: 1}}>      
+      <View style={styles.container}>
+        <View style={styles.profileImageContainer}>
+          <TouchableOpacity onPress={() => navigate('EmployerProfileScreen')}>
+            <Image source={require('./assets/ProfilePic.png')} style={styles.profileImageStyle} />
+            <View>
+              <Text> Name </Text>        
+              <Text> Job </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerBottom}>
+          <DrawerItems {...this.props} />
+          {/*
+          <TouchableOpacity
+            onPress={() => navigate('TabNavigator')}
+            style={styles.containerBottomItem}
+          >
+            <View style={styles.button}>
+              <Text style={styles.txtBottom}>Home</Text>
+            </View>
+          </TouchableOpacity>
+    
+          <TouchableOpacity
+            onPress={() => navigate('EmployerAccountScreen')}
+            style={styles.containerBottomItem}
+          >
+            <View style={styles.button}>
+              <Text style={styles.txtBottom}>Account</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => navigate('EmployerHistoryScreen')}
+            style={styles.containerBottomItem}
+          >
+            <View style={styles.button}>
+              <Text style={styles.txtBottom}>History</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => navigate('EmployerSettingsScreen')}
+            style={styles.containerBottomItem}
+          >
+            <View style={styles.button}>
+              <Text style={styles.txtBottom}>Settings</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => navigate('EmployerHelpScreen')}
+            style={styles.containerBottomItem}
+          >
+            <View style={styles.button}>
+              <Text style={styles.txtBottom}>Help</Text>
+            </View>
+          </TouchableOpacity>
+          */}
+        </View>
+      </View>
+      </SafeAreaView>
+    );    
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  },
+  containertopRow: {
+    marginTop: 10,
+    marginLeft: 10,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+  txtBottom: {
+    marginLeft: 10,
+    color: '#000000',
+    fontSize: 15,
+    fontWeight: '100'
+  },
+  imageTopRow: {
+    height: 20,
+    width: 20,
+    ...Platform.select({
+      ios: {
+        borderRadius: 20 / 2
+      },
+      android: {
+        borderRadius: 10
+      }
+    })
+  },
+  icon: {
+    height: 25,
+    width: 25,
+    marginRight: 10
+  },
+  button: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  },
+
+  containertopRowText: {
+    flexDirection: 'column',
+    marginLeft: 5
+  },
+
+  containerBottom: {
+    backgroundColor: '#ffffff'
+  },
+  containerBottomItem: {
+    padding: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    borderBottomColor: '#000000',
+    borderBottomWidth: 0.5
+  },
+  profileImageStyle: {
+    height: 120,
+    width: 120, 
+    borderRadius: 60
+  },
+  profileImageContainer: {
+    flexDirection: 'row',
+    height: 150, 
+    backgroundColor: 'white', 
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
--- Navigators.tsx
+++ Navigators.tsx
@@ -29,6 +29,7 @@ import { EmployerProfileScreen } from "./Screens/EmployerProfileScreen";
 import { EmployerSettingsScreen } from "./Screens/EmployerSettingsScreen";
 import { EmployerHistoryScreen } from "./Screens/EmployerHistoryScreen";
 import { EmployerHelpScreen } from "./Screens/EmployerHelpScreen";
+import { CustomDrawerComponent } from "./CustomDrawerComponent";
 
 const getTabBarIcon = (navigation, focused, tintColor) => {
   const { routeName } = navigation.state;
@@ -48,24 +49,6 @@ const getTabBarIcon = (navigation, focused, tintColor) => {
   return <IconComponent name={iconName} size={25} color={tintColor} />;
 };
 
-
-const customeDrawerComponent = (props) => (
-  <SafeAreaView style={{flex: 1}}>
-    <View style={styles.ProfileImageContainer}>
-      <TouchableOpacity onPress={() => props.navigation.navigate('EmployerProfileScreen')}>
-        <Image source={require('./assets/ProfilePic.png')} style={styles.ProfileImageStyle} />
-        <View>
-          <Text> Name </Text>        
-          <Text> Job </Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-    <ScrollView>
-      <DrawerItems {...props} />
-    </ScrollView>
-  </SafeAreaView>
-)
-
 const TabNavigator = createBottomTabNavigator(
   {
     Home: { screen: EmployerHomeScreen },
@@ -74,7 +57,6 @@ const TabNavigator = createBottomTabNavigator(
     More: { screen: EmployerMoreScreen }
   },
   {
-
     defaultNavigationOptions: ({navigation}) => ({
       tabBarIcon: ({ focused, tintColor }) =>
         getTabBarIcon(navigation, focused, tintColor)
@@ -88,15 +70,48 @@ const TabNavigator = createBottomTabNavigator(
   }
 );
 
+//Stack for putting into drawer, will use to navigate inside 
+//Custom component
+
+/* EXPLANATION FOR SAMMY
+  This stack is used in the DrawerNavigator to help render the Profile Screen. The only way for a touchOpacity tag 
+  to navigate to a different screen is if the touchOpacity is referring to something in a stack. In the CustomDrawerComponent
+  js file on line 35 it is directly linking EmployerProfileScreen from the DrawerStackNavigator in this file. But this file is used
+  as a custom component under contentComponent in the DrawerNavigator in this file. 
+
+  There are two versions of this:
+    1: The version that is with the commented out lines. This version doesnt add everything in the stack, instead uses 
+    the DrawerItems tag on line 44 to create each individual menu item in drawer. This is what is currently rendered in expo if 
+    you pull from this branch and dont make any changes
+
+    2: Uncomment everything from line 92 to 98 in this file and 45 to 90 in CustomDrawerComponent.js file. This is creating
+    each menu item by hand using touchOpacity and it outlines even further how using the stack (DrawerStackNavigator) is needed
+    for the touchOpacity buttons.
+
+  I think version one is what we want more since we want to be able to pull the drawer when we are on the screens that we get to 
+  through the drawer. But im not sure if its bad coding practice yet for what i've done for version one. 
+*/
+const DrawerStackNavigator = createStackNavigator({
+  /*
+  TabNavigator,
+  EmployerAccountScreen,
+  EmployerHistoryScreen,
+  EmployerSettingsScreen,
+  EmployerHelpScreen,
+  */
+  EmployerProfileScreen
+})
+
 const DrawerNavigator = createDrawerNavigator({
-  Home: { 
-    screen: TabNavigator},
+
+  Home: { screen: TabNavigator},
   Account: { screen: EmployerAccountScreen },
   History: { screen: EmployerHistoryScreen },
   Settings: { screen: EmployerSettingsScreen },
-  Help: { screen: EmployerHelpScreen }
+  Help: { screen: EmployerHelpScreen },
+  DrawerStackNavigator
 }, {
-  contentComponent: customeDrawerComponent
+  contentComponent: props => <CustomDrawerComponent {...props} />
 });
 
 const ProfileImageStyle: ImageStyle = {
