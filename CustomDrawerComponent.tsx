18b76eee17c37bd9d4cb38ebfaae51aaf5deb28e
Sammy Najib <sammynajib@ou.edu>
2019-09-02 00:55:23 -0500

    adjust profile pic and text with

--- CustomDrawerComponent.tsx
+++ CustomDrawerComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {DrawerItems, DrawerItemsProps} from "react-navigation";
+import { DrawerItems, DrawerItemsProps } from "react-navigation";
 import {
   View,
   Text,
@@ -7,33 +7,36 @@ import {
   TouchableOpacity,
   StyleSheet,
   SafeAreaView,
+  ViewStyle,
+  TextStyle,
+  ImageStyle
 } from "react-native";
 
-
-export class CustomDrawerComponent extends React.Component <DrawerItemsProps>{
-
+export class CustomDrawerComponent extends React.Component<DrawerItemsProps> {
   public handleNavigate = () => () => {
-    this.props.navigation.navigate('EmployerProfileScreen');
+    this.props.navigation.navigate("EmployerProfileScreen");
     return;
   };
 
-  render() {        
+  render() {
     return (
-      
-      <SafeAreaView style={{flex: 1}}>      
-      <View style={styles.container}>
-        <View style={styles.profileImageContainer}>
-          <TouchableOpacity onPress={this.handleNavigate()}>
-            <Image source={require('./assets/ProfilePic.png')} style={styles.profileImageStyle} />
-            <View>
-              <Text> Name </Text>        
-              <Text> Job </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.containerBottom}>
-          <DrawerItems {...this.props} />
-          {/*
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <View style={styles.profileImageContainer}>
+            <TouchableOpacity onPress={this.handleNavigate()}>
+              <Image
+                source={require("./assets/ProfilePic.png")}
+                style={styles.profileImageStyle}
+              />
+              <View style={styles.textProfile}>
+                <Text style={{ fontWeight: "bold" }}> Name </Text>
+                <Text> Job </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={{ backgroundColor: "white" }}>
+            <DrawerItems {...this.props} />
+            {/*
           <TouchableOpacity
             onPress={() => navigate('TabNavigator')}
             style={styles.containerBottomItem}
@@ -75,71 +78,51 @@ export class CustomDrawerComponent extends React.Component <DrawerItemsProps>{
             style={styles.containerBottomItem}
           >
             <View style={styles.button}>
-              <Text style={styles.txtBottom}>Help</Text>
+              <Text style={styles.textBottom}>Help</Text>
             </View>
           </TouchableOpacity>
           */}
+          </View>
         </View>
-      </View>
       </SafeAreaView>
-    );    
+    );
   }
 }
 
+const container: ViewStyle = {
+  flex: 1,
+  backgroundColor: "#ffffff"
+};
+
+const profileImageContainer: ViewStyle = {
+  display: "flex",
+  flexDirection: "row",
+  height: 150,
+  backgroundColor: "white",
+  alignItems: "flex-start",
+  justifyContent: "flex-start"
+};
+
+const profileImageStyle: ImageStyle = {
+  alignSelf: "flex-start",
+  flex: 1,
+  height: 120,
+  width: 120,
+  borderRadius: 60,
+  paddingTop: 75,
+  marginLeft: 20
+};
+
+const textProfile: ImageStyle = {
+  alignSelf: "flex-end",
+  flex: 2,
+  marginLeft: 150,
+  marginTop: -75
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff'
-  },
-  containertopRow: {
-    marginTop: 10,
-    marginLeft: 10,
-    justifyContent: "center",
-    alignItems: 'center'
-  },
-  txtBottom: {
-    marginLeft: 10,
-    color: '#000000',
-    fontSize: 15,
-    fontWeight: '100'
-  },
-  icon: {
-    height: 25,
-    width: 25,
-    marginRight: 10
-  },
-  button: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
-  },
-
-  containertopRowText: {
-    flexDirection: 'column',
-    marginLeft: 5
-  },
-
-  containerBottom: {
-    backgroundColor: '#ffffff'
-  },
-  containerBottomItem: {
-    padding: 10,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    borderBottomColor: '#000000',
-    borderBottomWidth: 0.5
-  },
-  profileImageStyle: {
-    height: 120,
-    width: 120, 
-    borderRadius: 60
-  },
-  profileImageContainer: {
-    flexDirection: 'row',
-    height: 150, 
-    backgroundColor: 'white', 
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+  container,
+  profileImageContainer,
+  profileImageStyle,
+  textProfile
 });
