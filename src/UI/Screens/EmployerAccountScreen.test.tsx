fd65c679bc84cd9b5648d957b9cfd46f904e24fd
Sammy Najib <sammynajib@ou.edu>
2019-10-20 01:57:33 -0500

    fix test

--- src/UI/Screens/EmployerAccountScreen.test.tsx
+++ src/UI/Screens/EmployerAccountScreen.test.tsx
@@ -2,15 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { EmployerAccountScreen } from './EmployerAccountScreen';
 
-{
-  /* <LinearGradient /> */
-}
-// jest.mock('');
-
-jest.doMock('expo-linear-gradient', () => {
-  LinearGradient: () => 'LinearGradient';
-});
-
 it('should render with snapshot', () => {
   const employerAcctScreen = renderer.create(<EmployerAccountScreen />);
   expect(employerAcctScreen.toJSON()).toMatchSnapshot();
