7f8b6459404944279e6d39e8daab85d2770f6d3a
Sammy Najib <sammynajib@ou.edu>
2019-10-20 01:06:32 -0500

    fook

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ const { defaults } = require('jest-config');
 module.exports = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   verbose: true,
+  preset: 'react-native',
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
 };
--- src/UI/Screens/EmployerAccountScreen.test.tsx
+++ src/UI/Screens/EmployerAccountScreen.test.tsx
@@ -1,9 +1,8 @@
-// Copyright 2018 Prescryptive Health Inc
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { EmployerAccountScreen } from './EmployerAccountScreen';
 
 it('should render with snapshot', () => {
-  const pricingScreen = renderer.create(<EmployerAccountScreen />);
-  expect(pricingScreen.toJSON()).toMatchSnapshot();
+  const employerAcctScreen = renderer.create(<EmployerAccountScreen />);
+  expect(employerAcctScreen.toJSON()).toMatchSnapshot();
 });
