7b4d27bb66e484a56dd18f78ad246abc4c1d4cb3
Zaki <zkrefai@gmail.com>
2019-11-16 11:34:20 -0600

    Changed menu-button to Menu-Button

--- src/UI/App-Navigator/Stack/stack-navigator-tab-options.ts
+++ src/UI/App-Navigator/Stack/stack-navigator-tab-options.ts
@@ -1,29 +1,29 @@
 import {
   MenuButton,
   SandwichButton,
-  RegularButton,
-} from '../Components/menu-button';
-import { NavigationComponent } from 'react-navigation';
+  RegularButton
+} from "../Components/Menu-Button";
+import { NavigationComponent } from "react-navigation";
 
 export const stackNavigatorOptions = {
   navigationOptions: ({ navigation }: NavigationComponent) => ({
     headerLeft: MenuButton(navigation),
-    headerTransparent: 'true',
-  }),
+    headerTransparent: "true"
+  })
 };
 
 export const finderRegularStackNavigatorOptions = {
   navigationOptions: ({ navigation }: NavigationComponent) => ({
     headerLeft: MenuButton(navigation),
     headerRight: RegularButton(navigation),
-    headerTransparent: 'true',
-  }),
+    headerTransparent: "true"
+  })
 };
 
 export const finderSandwichStackNavigatorOptions = {
   navigationOptions: ({ navigation }: NavigationComponent) => ({
     headerLeft: MenuButton(navigation),
     headerRight: SandwichButton(navigation),
-    headerTransparent: 'true',
-  }),
+    headerTransparent: "true"
+  })
 };
