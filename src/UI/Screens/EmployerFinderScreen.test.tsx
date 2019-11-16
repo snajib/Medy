feb9f1db1332ae1207745b3d4b3df17f825f0f9c
Sammy Najib <sammynajib@ou.edu>
2019-11-16 15:54:06 -0600

    test upd

--- src/UI/Screens/EmployerFinderScreen.test.tsx
+++ src/UI/Screens/EmployerFinderScreen.test.tsx
@@ -5,13 +5,21 @@ import { View } from 'react-native';
 
 describe('EmployerFinderScreen', () => {
   it('should render views correctly', () => {
-    const finderScreen = renderer.create(<EmployerFinderScreen />);
+    const finderScreen = renderer.create(
+      <EmployerFinderScreen
+        EmployeeList={[{ description: '', jobTitle: '', name: '' }]}
+      />
+    );
 
-    expect(finderScreen.root.findAllByType(View).length).toBe(13);
+    expect(finderScreen.root.findAllByType(View).length).toBe(5);
   });
 
   it('should render with snapshot', () => {
-    const employerFindScreen = renderer.create(<EmployerFinderScreen />);
+    const employerFindScreen = renderer.create(
+      <EmployerFinderScreen
+        EmployeeList={[{ description: '', jobTitle: '', name: '' }]}
+      />
+    );
     expect(employerFindScreen.toJSON()).toMatchSnapshot();
   });
 });
